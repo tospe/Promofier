6a616f0267d3fc8c56331a967f84e59bad27c5cc
Tomás Sousa Pereira <up201507414@fe.up.pt>
2021-03-08 14:25:22 +0000

    Auxiliar files

--- promo_class.py
+++ promo_class.py
@@ -16,17 +16,17 @@ class PromotionDataset(torch.utils.data.Dataset):
 	def __getitem__(self,idx):
 		#load images and annotations
 		img_path = os.path.join(self.data_dir, "IMGS", self.imgs[idx])
-        img = Image.open(img_path).convert("RGB")
+		img = Image.open(img_path).convert("RGB")
 
-        #annots
-        annot_path = os.path.join(self.data_dir, "ANNOTATIONS", self.annotations[idx])
-        annots = ET.parse(annot_path)
-        objects = annots.getroot().findall('object')
-        num_objs = len(objects)
-        boxes = []
+		#annots
+		annot_path = os.path.join(self.data_dir, "ANNOTATIONS", self.annotations[idx])
+		annots = ET.parse(annot_path)
+		objects = annots.getroot().findall('object')
+		num_objs = len(objects)
+		boxes = []
 
-        #get all boxes
-        for o in objects:
+		#get all boxes
+		for o in objects:
 			xmin = int(o.find('bndbox').find('xmin').text)
 			xmax = int(o.find('bndbox').find('ymin').text)
 			ymin = int(o.find('bndbox').find('xmax').text)
@@ -34,24 +34,24 @@ class PromotionDataset(torch.utils.data.Dataset):
 			boxes.append([xmin, ymin, xmax, ymax])
 
 		boxes = torch.as_tensor(boxes, dtype=torch.float32) 	
- 		labels = torch.ones((num_objs,), dtype=torch.int64)
- 		image_id = torch.tensor([idx])
- 		area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+		labels = torch.ones((num_objs,), dtype=torch.int64)
+		image_id = torch.tensor([idx])
+		area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
 
 		# suppose all instances are not crowd
 		iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        target["image_id"] = image_id
-        target["area"] = area
-        target["iscrowd"] = iscrowd
+		target = {}
+		target["boxes"] = boxes
+		target["labels"] = labels
+		target["image_id"] = image_id
+		target["area"] = area
+		target["iscrowd"] = iscrowd
 
-        if self.transforms is not None:
-            img, target = self.transforms(img, target)
+		if self.transforms is not None:
+			img, target = self.transforms(img, target)
 
-        return img, target  
+		return img, target  
 
-   def __len__(self):
-        return len(self.imgs)        
+	def __len__(self):
+		return len(self.imgs)        
--- train.py
+++ train.py
@@ -15,24 +15,24 @@ import utils
 import transforms as T
 
 def build_model(num_classes):
-    # load an instance segmentation model pre-trained on COCO
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+	# load an instance segmentation model pre-trained on COCO
+	model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
-    # get the number of input features for the classifier
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
+	# get the number of input features for the classifier
+	in_features = model.roi_heads.box_predictor.cls_score.in_features
 
 	# replace the pre-trained head with a new one
 	model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
 def get_transform(train):
-    transforms = []
-    # converts the image, a PIL image, into a PyTorch Tensor
-    transforms.append(T.ToTensor())
-    if train:
-        # during training, randomly flip the training images
-        # and ground-truth for data augmentation
-        transforms.append(T.RandomHorizontalFlip(0.5))
-    return T.Compose(transforms)
+	transforms = []
+	# converts the image, a PIL image, into a PyTorch Tensor
+	transforms.append(T.ToTensor())
+	if train:
+		# during training, randomly flip the training images
+		# and ground-truth for data augmentation
+		transforms.append(T.RandomHorizontalFlip(0.5))
+	return T.Compose(transforms)
 
 if __name__ == "__main__":
 	# use our dataset and defined transformations
@@ -63,7 +63,7 @@ if __name__ == "__main__":
 	# and a learning rate scheduler which decreases the learning rate by
 	# 10x every 3 epochs
 	lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-	                                               step_size=3,
+		                                       step_size=3,
 	                                               gamma=0.1)
 
 	# number of epochs
